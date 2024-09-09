c9fc69fce3475b681ebaa3f707b8bf6195d7f310
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-09 10:24:19 +0300

    docstring

--- mail_merge.py
+++ mail_merge.py
@@ -1,15 +1,27 @@
 class MailMerge:
     def names(self):
+        """_Open txt of invited people and return the names_
+
+        Returns:
+            _list_: _Names_
+        """
         with open("./Input/Names/invited_names.txt", "r") as file:
             names = file.readlines()
         return names
 
-    def letter(self):
+    def letter(self)-> str:
+        """_Open txt for example output and return str of the letter_
+
+        Returns:
+            str: _example of output letter_
+        """
         with open("./Input/Letters/starting_letter.txt", "r") as file:
             letter = file.read()
         return letter
 
     def mail_merge(self):
+        """_Get the list of names and example letter, iterate through the names and write the ready to send letter_
+        """
         names = self.names()
         names = [name.removesuffix("\n") for name in names]
         letter = self.letter()
